de5cbe43ba006ab51a0a99b70fe02b244303b686
Andre Cardoso <andrecardosodev@gmail.com>
2015-10-14 00:19:40 -0300

    removido plugin embarcado do PagSeguro

--- app/Plugin/PagSeguro/Exemplos/Config/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    Configure::write('PagSeguro.credenciais', array(
-		  'email' => 'seu-email',
-		  'token' => 'seu-token'
-    ));
--- app/Plugin/PagSeguro/Exemplos/Controller/AppController.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-App::uses('Controller', 'Controller');
-
-class AppController extends Controller {
-    public $components = array('PagSeguro.Carrinho', 'PagSeguro.Notificacao', 'PagSeguro.Consulta');
-}
